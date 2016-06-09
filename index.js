6f7cc2b872980ce277c566710f0397a07cbad211
Trevor Livingston <tlivings@gmail.com>
2016-06-09 15:12:58 -0500

    semicolon

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const entries = function *(obj) {
     const keys = Object.keys(obj);
 
     for (let i = 0; i < keys.length; i++) {
-        const key = keys[i]
+        const key = keys[i];
 
         yield [key, obj[key]];
     }
